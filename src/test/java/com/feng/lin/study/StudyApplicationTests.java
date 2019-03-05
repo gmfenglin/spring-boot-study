4c0044e7eff71b9bf0c2e60113433ea779a9a719
fenglin <gmfenglin@gmail.com>
2019-03-05 14:48:56 +0800

    监控，热加载

--- src/test/java/com/feng/lin/study/StudyApplicationTests.java
+++ src/test/java/com/feng/lin/study/StudyApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class StudyApplicationTests {
 
 	@Test
 	public void contextLoads() {
